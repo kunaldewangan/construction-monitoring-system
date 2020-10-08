f495c3a306bb6a5efe9799d74b65d99b6836b64a
abhinav-p-tiwari <abhinavtiwari.kv@gmail.com>
2020-10-08 17:24:29 +0530

    index page of admin added

---
+++ admin/index.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Index</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+  <div class="container">
+	<div class="row m-2 p-2">
+		<div class="col-md-12 text-center">
+		<h6 class="display-4">Wants to Login?</h6>
+		</div>
+	</div>
+	
+    <div class="row m-2 p-2">
+	
+    <div class="col-md-4"></div>
+      <div class="col-md-4 text-center">
+        
+      </div>
+      <div class="col-md-4"></div>
+    </div>
+
+    <div class="row text-center">
+      <div class="col-md-4">
+
+        <div class="card" style="width: 18rem;">
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top img-thumbnail" src="../includes/images/login.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">User Login</h5>
+              <a href="user/user_login.php" class="card-link">
+                <button class="btn btn-primary">Login</button>
+              </a>
+            </div>
+          </div>
+      </div>
+
+    </div>
+    <!-- end of col-md-4 -->
+
+      <div class="col-md-4">
+
+        <div class="card" style="width: 18rem;">
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top img-thumbnail" src="../includes/images/login.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Contractor Login</h5>
+              <a href="contractor/contractor_login.php" class="card-link">
+                <button class="btn btn-primary">Login</button>
+              </a>
+            </div>
+          </div>
+      </div>
+
+    </div>
+    <!-- end of col-md-4 -->
+      <div class="col-md-4">
+
+        <div class="card" style="width: 18rem;">
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top img-thumbnail" src="../includes/images/login.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Supervisor Login</h5>
+              <a href="supervisor/supervisor_login.php" class="card-link">
+                <button class="btn btn-primary">Login</button>
+              </a>
+            </div>
+          </div>
+      </div>
+
+    </div>
+    <!-- end of col-md-4 -->
+
+  </div>
+  <!--end of row-->
+
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
